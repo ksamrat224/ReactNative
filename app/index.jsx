ade093ef2f268f24f9b744707953ca6582f320bc
ksamrat224 <ksamrat224@gmail.com>
2025-05-30 10:18:41 +0545

    tailwind setup finally done

--- app/index.jsx
+++ app/index.jsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>add nativewind for tailwind</Text>
+      <Text className="text-yellow-500">add nativewind  tailwid</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
