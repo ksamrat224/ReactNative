6217af0b91bc9fe8a38893d7be76c28f9e0f7239
ksamrat224 <ksamrat224@gmail.com>
2025-06-06 21:03:15 +0545

    Colors.js for color

---
+++ assets/Colors.js
@@ -0,0 +1,25 @@
+const tintColorLight = "#2563eb";  
+const tintColorDark = "#93c5fd";   
+const primary = "#f97316";    
+const secondary = "#1f2937";       
+
+export const Colors = {
+  light: {
+    text: "#111827",              
+    background: "#f9fafb",    
+    tint: tintColorLight,
+    icon: "#6b7280",        
+    tabIconDefault: "#9ca3af",   
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#f3f4f6",             
+    background: "#111827",     
+    tint: tintColorDark,
+    icon: "#9ca3af",            
+    tabIconDefault: "#6b7280",    
+    tabIconSelected: tintColorDark,
+  },
+  PRIMARY: primary,
+  SECONDARY: secondary,
+};
