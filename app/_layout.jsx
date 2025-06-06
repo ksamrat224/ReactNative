8a0d72176fc87cfe1a05b63e976d482e23d1ac54
ksamrat224 <ksamrat224@gmail.com>
2025-06-06 16:40:45 +0545

    selfclosing Stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,9 +2,5 @@ import { Stack } from "expo-router";
 import { Text } from "react-native";
 import "../global.css";
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Text> Hello World</Text>
-    </Stack>
-  );
+  return <Stack/>;
 }
