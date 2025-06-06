402a937211d29a5662024d2b557576df504058eb
ksamrat224 <ksamrat224@gmail.com>
2025-06-06 17:23:17 +0545

    headerShown to false

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,7 +4,7 @@ import { Tabs } from 'expo-router'
 
 const TabLayout = () => {
   return (
-   <Tabs>
+   <Tabs screenOptions={{headerShown:false}}>
     <Tabs.Screen name='home' options={{title:"Home"}}/>
     <Tabs.Screen name='history' options={{title:"History"}}/>
     <Tabs.Screen name='profile' options={{title:"Profile"}}/>
