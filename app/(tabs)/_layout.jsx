9e0f407e792f476bb907a16d3e85e391555c320b
ksamrat224 <ksamrat224@gmail.com>
2025-06-06 21:18:25 +0545

    tabs css

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,30 @@
-import { Tabs } from 'expo-router'
+import { Tabs } from "expo-router";
+import { Colors } from "../../assets/Colors";
 
 const TabLayout = () => {
   return (
-   <Tabs screenOptions={{headerShown:false}}>
-    <Tabs.Screen name='home' options={{title:"Home"}}/>
-    <Tabs.Screen name='history' options={{title:"History"}}/>
-    <Tabs.Screen name='profile' options={{title:"Profile"}}/>
-   </Tabs>
-  )
-}
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.PRIMARY,
+        tabBarInactiveTintColor: Colors.dark.text,
+        tabBarStyle: {
+          backgroundColor: Colors.SECONDARY,
+          height: 95,
+          paddingBottom: 10,
+          paddingTop: 5,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Tabs.Screen name="home" options={{ title: "Home" }} />
+      <Tabs.Screen name="history" options={{ title: "History" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+};
 
-export default TabLayout
+export default TabLayout;
