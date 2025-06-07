fc9e349b04b4dbf841a7cd10b296e6ac999fc406
ksamrat224 <ksamrat224@gmail.com>
2025-06-07 14:58:09 +0545

    custom background color added

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,13 @@
 import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-// import logo from "../assets/images/dinetimelogo.png";
-const logo = require("../assets/images/dinetimelogo.png"); //different import method for local images
+import logo from "../assets/images/dinetimelogo.png";
+// const logo = require("../assets/images/dinetimelogo.png"); //different import method for local images
 
 export default function Index() {
   const router = useRouter();
   return (
-    <SafeAreaView>
+    <SafeAreaView className={`bg-[#1f2937]`}>
       <ScrollView contentContainerStyle={{height: "100%"}}>
         <View>
           <Image source={logo} style={{width:"300",height:"300"}}/>
