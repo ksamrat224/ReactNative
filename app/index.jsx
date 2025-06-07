c10eb402107191f34bcf760a6cd9cab1db417898
ksamrat224 <ksamrat224@gmail.com>
2025-06-07 15:27:36 +0545

    onpress to home router

--- app/index.jsx
+++ app/index.jsx
@@ -13,6 +13,7 @@ import logo from "../assets/images/dinetimelogo.png";
 // const logo = require("../assets/images/dinetimelogo.png"); //different import method for local images
 
 export default function Index() {
+  const router = useRouter();
   return (
     <SafeAreaView className={`bg-[#1f2937]`}>
       <StatusBar barStyle="light-content" backgroundColor={"#1f2937"} />
@@ -20,7 +21,7 @@ export default function Index() {
         <View className="m-2 flex items-center justify-center">
           <Image source={logo} style={{ width: "300", height: "300" }} />
           <View className="w-3/4">
-            <TouchableOpacity className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg">
+            <TouchableOpacity onPress={()=>router.push("/home")} className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg">
               <Text className="text-center text-base font-bold">
                 Sign Up
               </Text>
