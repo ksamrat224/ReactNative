56a8ea75fb921d5e55e5f32436df3861e41496b9
ksamrat224 <ksamrat224@gmail.com>
2025-06-07 15:01:54 +0545

    changed color of status bar

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { Image, ScrollView, StatusBar, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../assets/images/dinetimelogo.png";
 // const logo = require("../assets/images/dinetimelogo.png"); //different import method for local images
@@ -8,6 +8,7 @@ export default function Index() {
   const router = useRouter();
   return (
     <SafeAreaView className={`bg-[#1f2937]`}>
+      <StatusBar barStyle="light-content" backgroundColor={"#3e3df3"} />
       <ScrollView contentContainerStyle={{height: "100%"}}>
         <View>
           <Image source={logo} style={{width:"300",height:"300"}}/>
