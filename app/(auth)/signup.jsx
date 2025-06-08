ad8f23dc98d23f919db3aeaa3be771b2fdbf6a4a
ksamrat224 <ksamrat224@gmail.com>
2025-06-08 21:27:02 +0545

    unnecessay components removed

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -22,40 +22,6 @@ const Signup = () => {
           <Text className="text-white text-lg text-center font-bold mb-10">
             Welcome to DineTime
           </Text>
-          <View className="w-3/4">
-            <TouchableOpacity
-              onPress={() => router.push("/signup")}
-              className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg"
-            >
-              <Text className="text-center text-lg font-bold">Sign Up</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => router.push("/home")}
-              className="bg-[#1f2937] border border-[#f49b33]  p-4 my-2 rounded-lg shadow-lg max-w-fit"
-            >
-              <Text className="text-center text-lg font-semibold text-[#f49b33]">
-                Sign In as a guest
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <Text className="text-white text-lg text-center my-4 font-semibold">
-              <View className="border-b-2 border-[#f49b33] p-2 mb-1 w-24" /> or{" "}
-              <View className="border-b-2 border-[#f49b33] p-2 mb-1 w-24" />
-            </Text>
-            <TouchableOpacity
-              className="flex flex-row items-center justify-center"
-              onPress={() => router.push("/signin")}
-            >
-              <Text className="text-white font-semibold ">
-                Already a User?{" "}
-              </Text>
-              <Text className="text-[#f49b33] font-semibold text-base  underline">
-                Sign In
-              </Text>
-            </TouchableOpacity>
-          </View>
         </View>
         <View className="flex-1">
           <Image
@@ -64,6 +30,12 @@ const Signup = () => {
             resizeMode="contain"
           />
         </View>
+        <TouchableOpacity
+          onPress={() => router.push("/signup")}
+          className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg"
+        >
+          <Text className="text-center text-lg font-bold">Sign Up</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
