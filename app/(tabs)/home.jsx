2537a605f4e5aefd52f6e2e435a423361ac5bf26
ksamrat224 <ksamrat224@gmail.com>
2025-06-10 16:17:22 +0545

    home basic structuring and css

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../../assets/images/dinetimelogo.png";
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ backgroundColor: "#1f2937" }}>
+      <View className="flex items-center">
+        <View className="bg-[#6b7280] w-11/12 rounded-lg shadow-lg justify-between items-center">
+          <View className="flex flex-row">
+            <Text>Welcome to {""} </Text>
+            <Image source={logo} resizeMode="cover" className={"w-20 h-12"} />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default home
+export default home;
