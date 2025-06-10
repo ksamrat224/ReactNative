9d63f5ce48c97eda639e5b71ff56fe1dcc0bff02
ksamrat224 <ksamrat224@gmail.com>
2025-06-10 08:18:35 +0545

    handleSubmit and css modified

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -25,61 +25,62 @@ const Signup = () => {
           <Text className="text-white text-lg text-center font-bold mb-10">
             Let's get you started.
           </Text>
+
+          <View className="w-5/6 ">
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={""}
+              onSubmit={handleSignup}
+            >
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                touched,
+              }) => (
+                <View className="w-full">
+                  <Text>Email:</Text>
+                  <TextInput
+                    className="h-15 border border-white text-white rounded px-2 "
+                    keyboardType="email-address"
+                    onChangeText={handleChange("email")}
+                    value={values.email}
+                    onBlur={handleBlur("email")}
+                  />
+                  {touched.email && errors.email && (
+                    <Text className="text-red-600 text-xs mb-2">
+                      {errors.email}{" "}
+                    </Text>
+                  )}
+
+                  <Text>Password:</Text>
+                  <TextInput
+                    className="h-15 border border-white text-white rounded px-2 "
+                    secureTextEntry
+                    onChangeText={handleChange("password")}
+                    value={values.password}
+                    onBlur={handleBlur("password")}
+                  />
+                  {touched.password && errors.password && (
+                    <Text className="text-red-600 text-xs mb-2">
+                      {errors.password}{" "}
+                    </Text>
+                  )}
+                  <TouchableOpacity
+                    onPress={handleSubmit}
+                    className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg"
+                  >
+                    <Text className="text-center text-lg font-bold">
+                      Sign Up
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </Formik>
+          </View>
         </View>
-
-        <View className="w-5/6 ">
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={""}
-            onSubmit={handleSignup}
-          >
-            {({
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              values,
-              errors,
-              touched,
-            }) => (
-              <View className="w-full">
-                <Text>Email:</Text>
-                <TextInput
-                  className="h-15 border border-white text-white rounded px-2 "
-                  keyboardType="email-address"
-                  onChangeText={handleChange("email")}
-                  value={values.email}
-                  onBlur={handleBlur("email")}
-                />
-                {touched.email && errors.email && (
-                  <Text className="text-red-600 text-xs mb-2">
-                    {errors.email}{" "}
-                  </Text>
-                )}
-
-                <Text>Password:</Text>
-                <TextInput
-                  className="h-15 border border-white text-white rounded px-2 "
-                  secureTextEntry
-                  onChangeText={handleChange("password")}
-                  value={values.password}
-                  onBlur={handleBlur("password")}
-                />
-                {touched.password && errors.password && (
-                  <Text className="text-red-600 text-xs mb-2">
-                    {errors.password}{" "}
-                  </Text>
-                )}
-                <TouchableOpacity
-                  onPress={() => router.push("/signup")}
-                  className="bg-[#f49b33] text-black p-4 my-2 rounded-lg shadow-lg"
-                >
-                  <Text className="text-center text-lg font-bold">Sign Up</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </Formik>
-        </View>
-
         <View className="flex-1">
           <Image
             source={frame}
