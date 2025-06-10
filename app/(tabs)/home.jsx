4a5874d2eff7387570664f8ffb6c9f349e64e2f2
ksamrat224 <ksamrat224@gmail.com>
2025-06-10 16:48:00 +0545

    fix: adjust welcome card sizing and layout,plus scrollView added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { Image, Platform, Text, View } from "react-native";
+import { Image, Platform, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/dinetimelogo.png";
 
@@ -6,24 +6,22 @@ const home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "#1f2937" }}>
       <View className="flex items-center">
-        <View className="bg-[#4b5563] mt-4 w-11/12 rounded-lg shadow-lg justify-between items-center">
-          <View className="flex flex-row">
+        <View className="bg-[#4b5563] mt-4 w-11/12 rounded-lg shadow-lg items-center px-4 py-3">
+          <View className="flex flex-row items-center">
             <Text
-              className={`text-base h-10 align-middle text-white font-bold ${
-                Platform.OS === "android" ? "pt-2.5" : "pt-2"
+              className={`text-base text-white font-bold ${
+                Platform.OS === "android" ? "pt-0.5" : ""
               }`}
             >
-              {" "}
-              Welcome to {""}
+              Welcome to{" "}
             </Text>
-            <Image
-              source={logo}
-              resizeMode="cover"
-              className={"w-20 h-12 mt-1"}
-            />
+            <Image source={logo} resizeMode="cover" className="w-20 h-12" />
           </View>
         </View>
       </View>
+      <ScrollView>
+        
+      </ScrollView>
     </SafeAreaView>
   );
 };
