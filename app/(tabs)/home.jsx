21ec1d2d3958d5653b20fa8c2250460f65aaebcf
ksamrat224 <ksamrat224@gmail.com>
2025-06-10 16:36:24 +0545

    refactor: platform-based padding logic using className

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Image, Platform, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/dinetimelogo.png";
 
@@ -6,10 +6,21 @@ const home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "#1f2937" }}>
       <View className="flex items-center">
-        <View className="bg-[#6b7280] w-11/12 rounded-lg shadow-lg justify-between items-center">
+        <View className="bg-[#4b5563] mt-4 w-11/12 rounded-lg shadow-lg justify-between items-center">
           <View className="flex flex-row">
-            <Text>Welcome to {""} </Text>
-            <Image source={logo} resizeMode="cover" className={"w-20 h-12"} />
+            <Text
+              className={`text-base h-10 align-middle text-white font-bold ${
+                Platform.OS === "android" ? "pt-2.5" : "pt-2"
+              }`}
+            >
+              {" "}
+              Welcome to {""}
+            </Text>
+            <Image
+              source={logo}
+              resizeMode="cover"
+              className={"w-20 h-12 mt-1"}
+            />
           </View>
         </View>
       </View>
