15693b0de51a67738f898a69f5d9f4a92d80fd32
ksamrat224 <ksamrat224@gmail.com>
2025-06-12 10:32:23 +0545

    carousel data upload

--- config/bulkUpload.js
+++ config/bulkUpload.js
@@ -1,14 +1,14 @@
 import { collection, doc, setDoc } from "firebase/firestore";
-import { restaurants } from "../store/restaurant";
+import { carouselImages } from "../store/restaurant";
 import { db } from "./firebaseConfig";
 
-const restaurantData = restaurants;
+const restaurantData = carouselImages;
 
 const uploadData = async () => {
   try {
     for (let i = 0; i < restaurantData.length; i++) {
       const restaurant = restaurantData[i];
-      const docRef = doc(collection(db, "restaurants"), `restaurant_${i + 1}`);
+      const docRef = doc(collection(db, "carousel"), `carousel_${i + 1}`);
       await setDoc(docRef, restaurant);
     }
     console.log("Data uploaded successfully!");
