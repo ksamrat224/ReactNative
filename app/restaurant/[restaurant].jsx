f5ed6e502203ac608be7846fbf6c7daad206f293
ksamrat224 <ksamrat224@gmail.com>
2025-06-12 20:27:20 +0545

    feat: display restaurant name using dynamic route param

--- app/restaurant/[restaurant].jsx
+++ app/restaurant/[restaurant].jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useLocalSearchParams } from 'expo-router'
 
 const Restaurant = () => {
+    const {restaurant}= useLocalSearchParams();
   return (
-    <View>
-      <Text>Restaurant</Text>
-    </View>
+   <SafeAreaView>
+    <Text>{restaurant}</Text>
+   </SafeAreaView>
   )
 }
 
