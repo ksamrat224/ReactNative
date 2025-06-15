de4ac09aa2ecd6874051064cb31b3d017a586a17
ksamrat224 <ksamrat224@gmail.com>
2025-06-15 15:55:14 +0545

    GuestPicker component skeleton

---
+++ components/restaurant/GuestPicker.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const GuestPicker = () => {
+  return (
+    <View>
+      <Text>GuestPicker</Text>
+    </View>
+  );
+};
+
+export default GuestPicker;
