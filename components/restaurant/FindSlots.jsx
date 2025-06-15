2c1b0427160ba33a5151730dd62565fe28420484
ksamrat224 <ksamrat224@gmail.com>
2025-06-15 22:13:59 +0545

    Update layout for FindSlots and BookSlots buttons

--- components/restaurant/FindSlots.jsx
+++ components/restaurant/FindSlots.jsx
@@ -9,18 +9,27 @@ const FindSlots = ({
   selectedNumber,
 }) => {
   const [slotsVisible, setSlotsVisible] = useState(false);
-  const handlePress=()=>{
+  const handlePress = () => {
     setSlotsVisible(!slotsVisible);
-  }
+  };
 
   return (
     <View className="flex-1">
       <View className={`flex ${selectedSlot != null && "flex-row"}`}>
-        <TouchableOpacity onPress={handlePress}>
-          <Text className="text-center text-lg font-semibold  bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">
-            Find Slots
-          </Text>
-        </TouchableOpacity>
+        <View className={`${selectedSlot != null && flex - 1}`}>
+          <TouchableOpacity onPress={handlePress}>
+            <Text className="text-center text-lg font-bold  bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">
+              Find Slots
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View className="flex-1">
+          <TouchableOpacity onPress={handlePress}>
+            <Text className="text-center text-lg font-bold  bg-[#f49b33] p-2 my-3 mx-2 rounded-lg text-white">
+              Book Slots
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
