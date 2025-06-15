ad5f33a989feebf25bbfad9b73d83d5b7d4eadf4
ksamrat224 <ksamrat224@gmail.com>
2025-06-15 16:26:53 +0545

    increment and decrement function  implemented using state

--- components/restaurant/GuestPicker.jsx
+++ components/restaurant/GuestPicker.jsx
@@ -1,22 +1,29 @@
 import { Text, TouchableOpacity, View } from "react-native";
 
-const GuestPicker = ({selectedNumber}) => {
-    const increment = () => {
-    }
-    const decrement = ()=>{
-        
-    }
+const GuestPicker = ({ selectedNumber, setSelectedNumber }) => {
+  const increment = () => {
+    if(selectedNumber<10)
+    setSelectedNumber(selectedNumber + 1);
+  };
+  const decrement = () => {
+    if(selectedNumber>1)
+    setSelectedNumber(selectedNumber - 1);
+  };
   return (
     <View className="flex flex-row items-center rounded-lg text-white text-base">
       <TouchableOpacity onPress={decrement} className="rounded">
-        <Text className="text-white text-lg border border-[#f49b33]">-</Text>
+        <Text className="text-white text-lg border border-[#f49b33] rounded-l-lg px-3">
+          -
+        </Text>
       </TouchableOpacity>
-      <Text className="px-3 text-white bg-[#474747] text-lg">
+      <Text className="px-3 text-white border border-[#474747] bg-[#474747] text-lg">
         {selectedNumber}
       </Text>
 
       <TouchableOpacity onPress={increment} className="rounded">
-        <Text className="text-white text-lg border border-[#f49b33]">+</Text>
+        <Text className="text-white text-lg border border-[#f49b33] rounded-r-lg px-3">
+          +
+        </Text>
       </TouchableOpacity>
     </View>
   );
